cfd2141ae9dabbde7238bedd4d4adddeedd1532e
Bladimir <bladimirsontay@gmail.com>
2024-07-29 21:29:25 -0600

    Crreacion de for Test

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ alert("Bienvenido a mi página web");
 function changebest(){
     document.getElementById("best").innerHTML = "El mejor sitio web";
 }
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
